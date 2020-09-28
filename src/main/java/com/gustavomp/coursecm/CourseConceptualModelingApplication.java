0d11f4357630c25c48117015204e2f146c88e607
Volaxy <guga.PRO.00@hotmail.com>
2020-09-28 10:11:08 -0300

    Project Created

---
+++ src/main/java/com/gustavomp/coursecm/CourseConceptualModelingApplication.java
@@ -0,0 +1,13 @@
+package com.gustavomp.coursecm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseConceptualModelingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseConceptualModelingApplication.class, args);
+	}
+
+}
