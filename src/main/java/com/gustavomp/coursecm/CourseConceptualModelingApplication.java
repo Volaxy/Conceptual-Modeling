107999bae3ebe012b10f30c7b2fac1db07f72ba5
Volaxy <guga.PRO.00@hotmail.com>
2020-09-28 14:35:06 -0300

    Automatic instantiation

--- src/main/java/com/gustavomp/coursecm/CourseConceptualModelingApplication.java
+++ src/main/java/com/gustavomp/coursecm/CourseConceptualModelingApplication.java
@@ -1,13 +1,31 @@
 package com.gustavomp.coursecm;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CourseConceptualModelingApplication {
+import com.gustavomp.coursecm.domain.Categoria;
+import com.gustavomp.coursecm.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CourseConceptualModelingApplication implements CommandLineRunner {
+
+	@Autowired
+	CategoriaRepository rep;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CourseConceptualModelingApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria c1 = new Categoria(null, "Informática");
+		Categoria c2 = new Categoria(null, "Escritório");
+		
+		rep.saveAll(Arrays.asList(c1, c2));
 	}
 
 }
